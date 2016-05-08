623cec7bb80e9259a04b2a043f389dcc99b08e42
lulala <lulalachen@gmail.com>
2016-05-08 23:23:30 +0800

    init

---
+++ src/controller.js
@@ -0,0 +1,192 @@
+var homeApp = angular.module('homeApp', ['ngRoute', 'ngStorage']);
+console.log('hello app')
+
+const APIUrl = 'https://crossorigin.me/https://art-festival.herokuapp.com';
+
+const categoriesPath = {
+  'learning': 'src/badges/learning.svg',
+  'health': 'src/badges/health.svg',
+  'entertainment': 'src/badges/entertainment.svg',
+  'affection': 'src/badges/affection.svg',
+  'career': 'src/badges/career.svg'
+}
+
+const chineseLabelOfCategory = {
+  'learning': '學習',
+  'health': '健康',
+  'entertainment': '娛樂',
+  'affection': '感情',
+  'career': '事業'
+}
+
+homeApp.controller('homeCtrl', function ($scope, $location, $http, $localStorage) {
+  $scope.currentUser = {}
+
+  $scope.lands = [];
+
+  console.log($scope.currentUser)
+
+  // User Authentication Section
+  $scope.checkLogin = function () {
+    return ($localStorage.user !== undefined)
+  }
+
+  $scope.localLogin = function () {
+    if ($scope.checkLogin()) {
+      $scope.currentUser = $localStorage.user;
+      console.log('login from local')
+      if (!$scope.currentUser.isAlive) {
+        console.log(`user ${$scope.currentUser._id} is dead.`)
+        window.location.href = '/';
+      }
+    }
+  }
+  $scope.localLogin();
+
+
+  $scope.login = function() {
+    // const userId = '10004'
+    $http
+    .get(APIUrl + '/user/data?user=' + $scope.userId)
+    .success(function(data){
+      if (data) {
+        $scope.currentUser = data
+        $localStorage.user = data
+      }
+    })
+    .error(function(err) {
+      console.log(err)
+    })
+  }
+
+  $scope.logout = function () {
+    delete $localStorage.user;
+    setTimeout(window.location.href = '/', 1000)
+  }
+
+  $scope.getChineseLabel = function (englishName) {
+    return chineseLabelOfCategory[englishName] || englishName
+  }
+
+  $scope.getBadgePath = function (englishName) {
+    return categoriesPath[englishName]
+  }
+
+  // Land Section
+  $http
+  .get(`${APIUrl}/land/data?land=all`)
+  // .get('./src/land.json')
+  .success(function (data) {
+    console.log('land load success')
+    $scope.lands = []
+    for (var i = 0; i < Object.keys(data).length; i++) {
+      var key = Object.keys(data)[i]
+      $scope.lands.push({
+        landId: data[key]._id,
+        status: data[key].owner._id > 0,
+        category: chineseLabelOfCategory[data[key].category],
+        content: data[key].content,
+        badgePath: categoriesPath[data[key].category],
+        interest: data[key].interest,
+        price: data[key].price
+      })
+    }
+  })
+  .error(function(err){
+    console.log(err)
+  })
+  $scope.errorMessage = '';
+
+  $scope.buyLand = function (landId) {
+    console.log(`${$scope.currentUser._id} buys ${landId} with ${$scope.bidTime}`);
+    // $scope.bidTime
+    $scope.errorMessage = ''
+    if (parseFloat($scope.bidTime) > 0) {
+      $http
+      .get(`${APIUrl}/land/buy?user=${$scope.currentUser._id}&land=${landId}&time=${$scope.bidTime}`)
+      .success(function (data) {
+        $('.ui.basic.modal')
+          .modal('hide')
+        ;
+      })
+      .error(function (err) {
+        console.log(err)
+        $('.ui.basic.modal')
+          .modal('hide')
+        ;
+      })
+    } else {
+      $scope.errorMessage = '花費時間必須大於零'
+      console.log($scope.errorMessage)
+    }
+  }
+
+  $scope.getLandInfo = function(landId) {
+
+    $scope.currentLand = getLand(landId)
+    console.log($scope.currentLand)
+    $('.ui.basic.modal')
+      .modal({
+        blurring: true
+      })
+      .modal('setting', 'transition', 'fade up')
+      .modal('show')
+    ;
+  }
+  var getLand = function(landId) {
+    for (var i = 0; i < $scope.lands.length; i++) {
+      if ($scope.lands[i].landId === landId){
+        return $scope.lands[i]
+      }
+    }
+  }
+  // Route NavBar Section
+  $scope.isActive = function(path) {
+    return $location.path() == path
+  }
+
+  // Time Section
+  $scope.getTimeLeft = function () {
+    $http
+    .get(`${APIUrl}/center`)
+    .success(function (data) {
+      console.log(data)
+    })
+    .error(function (err) {
+      console.log(err)
+    })
+  }
+
+});
+
+
+homeApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: './home.html',
+        controller: 'homeCtrl'
+      })
+      .when('/landmap', {
+        templateUrl: './landmap.html',
+        controller: 'homeCtrl'
+      })
+      .when('/login', {
+        templateUrl: './login.html',
+        controller: 'homeCtrl'
+      })
+      .when('/leaderboard', {
+        templateUrl: './leaderboard.html',
+        controller: 'homeCtrl'
+      })
+      .when('/holdings', {
+        templateUrl: './holdings.html',
+        controller: 'homeCtrl'
+      })
+      .when('/dead', {
+        templateUrl: './dead.html',
+        controller: 'homeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
